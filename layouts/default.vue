47d56b97c0043dc0f90eec0b3397727c0550658b
Murota Taiki <mrttik0508@gmail.com>
2020-01-25 12:20:20 +0900

    ヘッダの作成

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,53 +1,33 @@
 <template>
   <div>
-    <nuxt/>
+    <div class="header">
+      <span class="header-info">情報収集</span>
+      <span class="header-logout">ログアウト</span>
+    </div>
+    <div class="main-content">
+      <nuxt/>
+    </div>
   </div>
 </template>
 
-<style>
-html {
-  font-family: "Source Sans Pro", -apple-system, BlinkMacSystemFont, "Segoe UI", Roboto, "Helvetica Neue", Arial, sans-serif;
-  font-size: 16px;
-  word-spacing: 1px;
-  -ms-text-size-adjust: 100%;
-  -webkit-text-size-adjust: 100%;
-  -moz-osx-font-smoothing: grayscale;
-  -webkit-font-smoothing: antialiased;
-  box-sizing: border-box;
-}
-
-*, *:before, *:after {
-  box-sizing: border-box;
-  margin: 0;
-}
-
-.button--green {
-  display: inline-block;
-  border-radius: 4px;
-  border: 1px solid #3b8070;
-  color: #3b8070;
-  text-decoration: none;
-  padding: 10px 30px;
-}
-
-.button--green:hover {
-  color: #fff;
-  background-color: #3b8070;
-}
-
-.button--grey {
-  display: inline-block;
-  border-radius: 4px;
-  border: 1px solid #35495e;
-  color: #35495e;
-  text-decoration: none;
-  padding: 10px 30px;
-  margin-left: 15px;
-}
-
-.button--grey:hover {
-  color: #fff;
-  background-color: #35495e;
-}
-</style>
-
+ <style lang="scss">
+ body{
+   margin: 0;
+ }
+.header{
+  width: 100%;
+  height: 60px;
+  background-color: aqua;
+  .header-info {
+    display: inline-block;
+    margin: 0;
+    padding: 10px 0 10px 20px;
+    font-size: 25px;
+  }
+  .header-logout{
+    float: right;
+    padding: 10px;
+    color: gray;
+  }
+} 
+ </style>
